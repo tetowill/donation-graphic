5addf1d9382e4dc637c6da47a37661bd6d109121
Will Hutchinson <willhutch81@gmail.com>
2014-01-27 22:15:13 -0800

    Added all files and updated README

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ donation-graphic
 ================
 
 Animated donations graphic based on donation total
+
+This is set up using a form input to manually submit a total donation amount. On submission, the graphic will adjust to show the correct amount. The donations.js file that is being used is for the manual test. Realistically this should be pulling the current donation amount from a json feed and simply updating on page load. When you're set up to do that, you can use the donations-feed.js instead of donations.js and simply enter your donations feed url in the specified location for the donationURL variable. Then just remove the form being used for manual submission and update the graphic, widths, etc. for your own needs. You can also remove the startAmount and matched variables if you do not need those added to your donation total.
+
+I am using accounting.js v0.3.2, copyright 2011 Joss Crowcroft, MIT license, http://josscrowcroft.github.com/accounting.js, to format the dollar amounts being shown under the graphic.
---
+++ donations-feed.js
@@ -0,0 +1,81 @@
+$(function(){
+
+	var donationURL = "YOUR_DONATION_FEED_URL.jsonp";
+
+	$.getJSON(donationURL+"?callback=?", function(data) {
+   		feedAmount = data.donationTotalAmount;
+   		DisplayAmount();
+ 	});
+	
+	var feedAmount; // amount from donation feed or form input for testing
+	var startAmount = 0; // if there is a starting amount other than 0 add it here
+	var donated;
+	var matched; // if matching donations
+	var totalDM; // total donations + matched
+	var total;
+	var goal = 2000000; // total goal amount
+	var maxWidth = 586; // width of .donate-graphic
+	var amount;
+	var percentage;
+	var aniTime; // animation time
+	
+
+	function DisplayAmount()
+	{
+	
+		if (isNumber(feedAmount))
+		{
+			$("span.valid").text("Validated...").show().delay(1000).fadeOut(1000);
+			
+			// remove decimal from feed amount
+			donated = parseFloat(feedAmount);
+			
+			// for a 50% match
+			matched = (donated / 2);
+			
+			// add donated and matched
+			totalDM = (donated + matched);
+			
+			// add start amount
+			total = (totalDM + startAmount);
+			
+			// if total is less than goal get percentage
+			if (total < goal)
+			{
+				percentage = (total / goal);
+				amount = Math.round(maxWidth * percentage);
+			}
+			else
+				amount = maxWidth;
+			
+			// adjust animaton time
+			if (amount < 293)
+				aniTime = 1000;
+			else
+				aniTime = 2000;
+			
+			// animate donations graphic
+			$('.donate-graphic.front').animate({width: amount}, aniTime, 'linear');		
+
+			// add amount text
+			$('span.start-amount').text(accounting.formatMoney(startAmount));
+			$('span.donated').text(accounting.formatMoney(donated));
+			$('span.matched').text(accounting.formatMoney(matched));
+			$('span.total').text(accounting.formatMoney(total));
+			$('div.numbers').fadeIn(500);
+			
+			return false;
+		}
+		else
+		{
+			$("span.valid").text("Please enter a number").show().delay(1000).fadeOut(1000);
+			return false;
+		}
+		
+	}
+
+	// check for number
+	function isNumber(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+});
